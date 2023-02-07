b500c5acc14151898912270a78d2873074a5b2a7
talentservice129 <talentservice129@gmail.com>
2023-02-06 23:18:04 -0800

    Swagger Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'api'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
